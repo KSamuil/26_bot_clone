b39324b16848e289ae12c564bc6de9a98c7a65ef
KSamuil <ksuharev49@gmail.com>
2023-08-24 00:36:05 +0600

    hw5

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ TOKEN = ''
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 p = ''
 
 
